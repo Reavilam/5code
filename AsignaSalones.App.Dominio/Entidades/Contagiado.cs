7aadead1aabdc594f91262afc89f6dbed19d91ad
Alberto Serrato <albertosewrrato@hotmail.com>
2021-09-22 23:32:02 -0500

    prueba

--- AsignaSalones.App.Dominio/Entidades/Contagiado.cs
+++ AsignaSalones.App.Dominio/Entidades/Contagiado.cs
@@ -7,7 +7,7 @@ namespace AsignaSalones.App.Dominio
         public int id {get;set;}
         public Persona persona {get;set;}
         public DateTime fechaContagio {get;set;}
-        //public System.Collections.Generic.List<Sintomas> sintomas {get;set;}
+       
         public String sintomas {get;set;}
         public DateTime periodoAislamiento {get;set;}
         
