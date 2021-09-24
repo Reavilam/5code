8eaf99d8f14939594607a2093ee90249af8e3f4b
whenao31 <whenao86@gmail.com>
2021-09-24 17:50:34 -0500

    Added Repos: HorarioClase, PersonalAseo, Matricula

--- AsignaSalones.App.Dominio/Entidades/HorarioClase.cs
+++ AsignaSalones.App.Dominio/Entidades/HorarioClase.cs
@@ -7,7 +7,6 @@ namespace AsignaSalones.App.Dominio
         public int id { get; set; }
         public String nombre { get; set; }
         public Profesor profesor { get; set; }
-        //public List<Estudiante> estudiantes { get; set; }
         public int cantPersonas { get; set; }
         public Salon salonAsignado { get; set; }
         public DateTime hora { get; set; }
