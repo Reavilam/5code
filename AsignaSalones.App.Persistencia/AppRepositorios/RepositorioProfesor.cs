f17f18e1bcd38885a370176ba95eb0604d12de30
whenao31 <whenao86@gmail.com>
2021-09-29 17:33:20 -0500

    Repositorios Directivo, Estudiante y Profesor

--- AsignaSalones.App.Persistencia/AppRepositorios/RepositorioProfesor.cs
+++ AsignaSalones.App.Persistencia/AppRepositorios/RepositorioProfesor.cs
@@ -6,52 +6,54 @@ namespace AsignaSalones.App.Persistencia
 {
     public class RepositorioProfesor : IRepositorioProfesor
     {
-        // private readonly AppContext _appContext;
-        // public RepositorioProfesor(AppContext appContext)
-        // {
-        //     _appContext = appContext;
-        // }
+        private readonly AppContext _appContext;
+        public RepositorioProfesor(AppContext appContext)
+        {
+            _appContext = appContext;
+        }
 
-        // Profesor IRepositorioProfesor.AddProfesor(Profesor profesor)
-        // {
-        //     var profesorAdicionado = _appContext.profesores.Add(profesor) ;
-        //     _appContext.SaveChanges();
-        //     return profesorAdicionado.Entity ;
-        // }
+        Profesor IRepositorioProfesor.AddProfesor(Profesor profesor)
+        {
+            var profesorAdicionado = _appContext.Profesores.Add(profesor) ;
+            _appContext.SaveChanges();
+            return profesorAdicionado.Entity ;
+        }
 
-        // Profesor IRepositorioProfesor.UpdateProfesor(Profesor profesor)
-        // {
-        //     var profesorEncontrado = _appContext.profesores.FirstOrDefault(p => p.id == profesor.id);
-        //     if (profesorEncontrado != null)
-        //     {
-        //         profesorEncontrado.nombre = profesor.nombre;
-        //         profesorEncontrado.apellidos = profesor.apellidos;
-        //         profesorEncontrado.identificacion = profesor.identificacion;
-        //         profesorEncontrado.telefono = profesor.telefono;
-        //         profesorEncontrado.correo = profesor.correo;
-        //         _appContext.SaveChanges();
-        //     }
-        //     return profesorEncontrado; 
-        // }
+        Profesor IRepositorioProfesor.UpdateProfesor(Profesor profesor)
+        {
+            var profesorEncontrado = _appContext.Profesores.FirstOrDefault(p => p.id == profesor.id);
+            if (profesorEncontrado != null)
+            {
+                profesorEncontrado.nombre = profesor.nombre;
+                profesorEncontrado.apellidos = profesor.apellidos;
+                profesorEncontrado.identificacion = profesor.identificacion;
+                profesorEncontrado.edad = profesor.edad;
+                profesorEncontrado.estadoCovid = profesor.estadoCovid;
+                profesorEncontrado.materia = profesor.materia;
+                profesorEncontrado.departamento = profesor.departamento;
+                _appContext.SaveChanges();
+            }
+            return profesorEncontrado;
+        }
 
-        // void IRepositorioProfesor.DeleteProfesor(int idprofesor)
-        // {
-        //     var profesorEncontrado = _appContext.profesores.FirstOrDefault(p => p.id == idprofesor);
-        //     if (profesorEncontrado == null)
-        //         return;
-        //     _appContext.profesores.Remove(profesorEncontrado);
-        //     _appContext.SaveChanges();
-        // }
+        void IRepositorioProfesor.DeleteProfesor(int idProfesor)
+        {
+            var profesorEncontrado = _appContext.Profesores.FirstOrDefault(p => p.id == idProfesor);
+            if (profesorEncontrado == null)
+                return;
+            _appContext.Profesores.Remove(profesorEncontrado);
+            _appContext.SaveChanges();
+        }
 
-        // Profesor IRepositorioProfesor.GetProfesor(int idprofesor)
-        // {
-        //     var profesorEncontrado = _appContext.profesores.FirstOrDefault(p => p.id == idprofesor);
-        //     return profesorEncontrado;
-        // }
+        Profesor IRepositorioProfesor.GetProfesor(int idProfesor)
+        {
+            var profesorEncontrado = _appContext.Profesores.FirstOrDefault(p => p.id == idProfesor);
+            return profesorEncontrado;
+        }
 
-        // IEnumerable<Profesor> IRepositorioProfesor.GetAllProfesores()
-        // {
-        //     return _appContext.profesores;
-        // }
+         IEnumerable<Profesor> IRepositorioProfesor.GetAllProfesores()
+        {
+            return _appContext.Profesores;
+        }
     }
 }
